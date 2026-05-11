bf930150a2a46dc8eeabb61bbee40b25bfeaf506
gmanuel89 <manuel.galli@revvity.com>
2026-05-11 17:27:33 +0200

    Keep snap on Ubuntu to avoid breakages

--- linux_post-installation_script.sh
+++ linux_post-installation_script.sh
@@ -53,29 +53,6 @@ if [[ "${REPLY}" =~ ^[Yy]$ ]]; then
 
 	## Ubuntu
 	if [ "${OS}" = "Ubuntu" ]; then
-		## Remove snap and replace it with flatpak
-		sudo apt autoremove --purge firefox*
-		sudo snap remove --purge firefox
-		sudo snap remove --purge thunderbird
-		sudo snap remove --purge firmware-updater
-		sudo snap remove --purge snap-store
-		sudo snap remove --purge gnome-42-2204
-		sudo snap remove --purge gtk-common-themes
-		sudo snap remove --purge bare
-		sudo snap remove --purge snapd-desktop-integration
-		sudo snap remove --purge core22
-		sudo snap remove --purge snapd
-		sudo apt autoremove --purge snapd*
-		sudo apt-mark hold snapd
-		sudo rm -rf ~/snap
-		sudo rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd
-		sudo tee /etc/apt/preferences.d/nosnap.pref << 'EOF'
-Package: snapd
-Pin: release a=*
-Pin-Priority: -1
-EOF
-		sudo add-apt-repository ppa:mozillateam/ppa
-		sudo apt install firefox
 		sudo add-apt-repository -y ppa:flatpak/stable
 		sudo apt update && sudo apt install -y flatpak
 		flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
