7d50c1c8f238381491917fb1064b7b6da7283ec2
gmanuel89 <manuel.galli@revvity.com>
2026-06-04 14:56:48 +0200

    fixed flatkap installations

--- linux_post-installation_script.sh
+++ linux_post-installation_script.sh
@@ -215,7 +215,7 @@ if [[ "${REPLY}" =~ ^[Yy]$ ]]; then
 	## YES
 	if [[ "${REPLY}" =~ ^[Yy]$ ]]; then
 	flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-	flatpak install flathub -y org.onlyoffice.desktopeditors com.spotify.Client org.kde.kdenlive org.telegram.desktop org.shotcut.Shotcut com.valvesoftware.Steam org.videolan.VLC #org.libreoffice.LibreOffice com.bitwarden.desktop org.mozilla.firefox org.mozilla.Thunderbird com.brave.Browser net.codelogistics.webapps com.vivaldi.Vivaldi org.localsend.localsend_app org.jellyfin.JellyfinDesktop io.github.mhogomchungu.media-downloader us.zoom.Zoom
+	flatpak install flathub -y org.onlyoffice.desktopeditors com.spotify.Client org.kde.kdenlive org.telegram.desktop org.shotcut.Shotcut com.valvesoftware.Steam org.videolan.VLC org.libreoffice.LibreOffice org.localsend.localsend_app org.jellyfin.JellyfinDesktop io.github.mhogomchungu.media-downloader us.zoom.Zoom #com.bitwarden.desktop org.mozilla.firefox org.mozilla.Thunderbird com.brave.Browser net.codelogistics.webapps com.vivaldi.Vivaldi
 	flatpak install flathub -y org.gnome.Boxes
 	fi
 	
